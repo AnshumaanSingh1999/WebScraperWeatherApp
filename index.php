b334320fd1a4837d517776167e615e06deb76fa3
Anshumaan Singh <47323148+colonel1999@users.noreply.github.com>
2020-07-11 02:15:03 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,62 @@
+<?php include_once("script.php") ?>
+<html>
+    <head>
+        <title>WeatherAppWebScrapper</title>
+        <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">  
+        <style>
+            
+            .container{
+                text-align: center;
+                margin-top: 200px;
+                width: 450px;
+            }
+            .margin{
+                margin-top: 20px;
+            }
+        
+        
+        
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <h1>WeatherApp</h1>
+        <form class="margin">
+  <fieldset class="form-group">
+    <label for="city">Enter the City Name</label>
+    <input type="text" class="form-control" id="city" placeholder="Eg : Lucknow, Delhi, Vellore" name="city"  value = "<?php if (array_key_exists('city', $_GET)) {
+    echo $_GET['city']; 
+ }
+ ?>">
+      
+  </fieldset>
+  
+  
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+        
+        <?php 
+              
+              if ($weather) {
+                  
+                  echo '<div class="alert alert-success" role="alert">
+  '.$weather.'
+</div>';
+                  
+              } else if ($error) {
+                  
+                  echo '<div class="alert alert-danger" role="alert">
+  '.$error.'
+</div>';
+                  
+              }
+              
+              ?>
+    </div>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/js/bootstrap.min.js" integrity="sha384-vZ2WRJMwsjRMW/8U7i6PWi6AlO1L79snBrmgiDpgIWJ82z8eA5lenwvxbMV1PAh7" crossorigin="anonymous"></script>  
+    </body>
+</html>
---
+++ script.php
@@ -0,0 +1,46 @@
+<?php
+    $weather = "";
+    $error = "";
+    
+    if (array_key_exists('city', $_GET)) {
+        
+        $city = str_replace(' ', '', $_GET['city']);
+        
+        $file_headers = @get_headers("https://www.weather-forecast.com/locations/".$city."/forecasts/latest");
+        
+        if($file_headers[0] == 'HTTP/1.1 404 Not Found') {
+    
+            $error = "That city could not be found.";
+
+        } else {
+        
+        $forecastPage = file_get_contents("https://www.weather-forecast.com/locations/".$city."/forecasts/latest");
+        
+        $pageArray = explode('Weather Today</h2> (1–3 days)</div><p class="b-forecast__table-description-content"><span class="phrase">', $forecastPage);
+            
+        if (sizeof($pageArray) > 1) {
+        
+                $secondPageArray = explode('</span></p></td><td class="b-forecast__table-description-cell--js" colspan="9"><div class="b-forecast__table-description-title"><h2>', $pageArray[1]);
+            
+                if (sizeof($secondPageArray) > 1) {
+
+                    $weather = $secondPageArray[0];
+                    
+                } else {
+                    
+                    $error = "That city could not be found.";
+                    
+                }
+            
+            } else {
+            
+                $error = "That city could not be found.";
+            
+            }
+        
+        }
+        
+    }
+
+
+?>
